a51581715647d50cc645faab08ad428b1e72f8c3
rchazen <80608143+rchazen@users.noreply.github.com>
2022-10-25 18:08:51 -0700

    Project with some data

--- project/app.py
+++ project/app.py
@@ -4,8 +4,17 @@ from flask import request
 from flask import abort, render_template
 from flask_sqlalchemy import SQLAlchemy
 import json
+import pandas as pd
+
+df = pd.read_csv('nation.csv')
+
 
 app = Flask(__name__)
+
+with app.app_context():
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
+    db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
